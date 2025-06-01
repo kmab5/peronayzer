e11256bfb25824198fcb1afcacabd929b16934fb
Samuel Worash Getaneh <samuelworash@gmail.com>
2025-06-01 15:38:34 +0300

    header added

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-    alert("Hello World!");
+    const darkToggle = document.querySelector(".dark-toggle");
+    const menuToggle = document.querySelector(".menu-toggle");
+    const menu = document.querySelector(".menu");
+    const close = document.querySelector(".close");
+
+    darkToggle.addEventListener("click", () => {
+        document.body.classList.toggle("dark");
+        darkToggle.classList.toggle("active-icon");
+    });
+
+    menuToggle.addEventListener("click", () => {
+        menu.classList.add("menu-visible");
+    });
+
+    close.addEventListener("click", () => {
+        menu.classList.remove("menu-visible");
+    });
 });
